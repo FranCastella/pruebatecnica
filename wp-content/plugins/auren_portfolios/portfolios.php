6ae225fe88dbe7610dd116db51bf63638e095cbf
FranCastella <devriably@gmail.com>
2023-02-01 14:38:48 +0100

    sanitize

--- wp-content/plugins/auren_portfolios/portfolios.php
+++ wp-content/plugins/auren_portfolios/portfolios.php
@@ -29,7 +29,7 @@ class Portfolios{
               'hierarchical' => false,
               'has_archive' => true,
               'show_in_rest' => true,
-              'supports'=> array( 'title', 'editor', 'author', 'thumbnail','excerpt','custom-fields','comments')
+              'supports'=> array( 'title', 'editor', 'author', 'thumbnail','excerpt','custom-fields','comments','taxonomy')
           ));
         //Añadir categorias personalizadas solo para portfolio (no usar por defecto)
         register_taxonomy('portfolios_category','portfolio',array('hierarchical' => true, 'label' => 'Categorías', 'query_var'=> true, 'rewrite'=> true,'show_admin_column'=>true));
@@ -42,16 +42,22 @@ class Portfolios{
     }
 
     function ajustes(){
+        settings_errors(); // esto es añadido automaticamente en el menu ajustes pero no en los demás
         add_settings_section('portfolios_first_section', null, null,'portfolios-pagina-ajustes');
 
-        add_settings_field('portfolio_filtro','Añadir filtro JS',array($this, 'ajustes_filtro_HTML'),'portfolios-pagina-ajustes','portfolios_first_section');
-        register_setting('portfoliosplugin','portfolios_filtro',array('sanitize_callback' => array($this, 'sanitizeJS'), 'default' => '0'));
+        add_settings_field('portfolio_orden','Orden de los portfolios por fecha',array($this, 'ajustes_orden_HTML'),'portfolios-pagina-ajustes','portfolios_first_section');
+        register_setting('portfoliosplugin','portfolios_orden',array('sanitize_callback' => array($this, 'sanitizeOrden'), 'default' => '0'));
 
-        add_settings_field('portfolio_catergorias','Categorías a añadir en el filtro (separar por una coma)',array($this, 'ajustes_categorias_HTML'),'portfolios-pagina-ajustes','portfolios_first_section');
-        register_setting('portfoliosplugin','portfolios_categorias',array('sanitize_callback' => array($this, 'sanitizeJS'), 'default' => ''));
+        add_settings_field('portfolio_filtro','Añadir filtro JS',array($this, 'ajustes_filtro_HTML'),'portfolios-pagina-ajustes','portfolios_first_section');
+        register_setting('portfoliosplugin','portfolios_filtro',array('sanitize_callback' => array($this, 'sanitizeFiltro'), 'default' => '0'));
+        
+        add_settings_field('portfolios_categorias','Categorías a añadir en el filtro (separar por una coma)',array($this, 'ajustes_categorias_HTML'),'portfolios-pagina-ajustes','portfolios_first_section');
+        register_setting('portfoliosplugin','portfolios_categorias',array('sanitize_callback' => array($this, 'sanitizeTextArea'), 'default' => ''));
     }
 
-    function nuestroHTML(){ ?>
+    function nuestroHTML(){
+        ?>
+
         <div class="wrap">
             <h1>Ajustes Portfolios</h1>
             <form action="options.php" method="POST">
@@ -65,30 +71,60 @@ class Portfolios{
      <?php   }
 
     function ajustes_filtro_HTML(){ ?>
-    <input type="hidden" name="portfolios_filtro" value="0">
-    <input type="checkbox" name="portfolios_filtro" value="1" <?php  checked(1, get_option('portfolios_filtro'), true);  ?> />
+        <input type="hidden" name="portfolios_filtro" value="0">
+        <input type="checkbox" name="portfolios_filtro" value="1" <?php  checked(1, get_option('portfolios_filtro'), true);  ?> />
     <?php }
 
-    function ajustes_categorias_HTML(){ ?>
-        <textarea name="portfolios_categorias" rows="6" cols="100" maxlength="120"><?php  echo get_option('portfolios_categorias')?></textarea>
+    function ajustes_orden_HTML(){ ?>
+        <select name="portfolios_orden">
+            <option value="0" <?php selected(get_option('portfolios_orden'),'0') ?> > <?php echo __('DESC.', 'wcpdomain') ?></option>
+            <option value="1" <?php selected(get_option('portfolios_orden'),'1') ?> ><?php echo __('ASC.', 'wcpdomain') ?></option>
+        </select>
+    <?php }
+
+    function ajustes_categorias_HTML(){
+        $lista_categorias = get_terms( array('taxonomy' => 'portfolios_category','hide_empty' => false )); //recoge categorias asociadas a portfolios y no filtra vacias
+        ?>
+        <textarea name="portfolios_categorias" rows="5" cols="100" ><?php echo  get_option('portfolios_categorias')?></textarea>
+        <select multiple disabled style="position:fixed"><?php
+        foreach($lista_categorias as $categoria){
+            echo "<option value=".$categoria->term_id.">".$categoria->name."</>";
+        } ?>
+        </select>
         <?php }
 
-    function sanitizeJS($input){
+    function sanitizeOrden($input){
         if($input !='0' AND $input !='1'){
             //1 setting asociado 2 codigo error(no importa) 3 mensaje de error
-            add_settings_error('portfolios_js','portfolios_soporte_js_error','Solo puedes elegir Si o No');
-            return get_option('portfolios_js');
+            add_settings_error('portfolios_orden','portfolios_orden_error','Solo puedes elegir esas 2');
+            return get_option('portfolios_orden');
         }
         return $input;
     }
-
-
-
-
-
-
-
-
+    function sanitizeFiltro($input){
+        if($input !='0' AND $input !='1'){
+            //1 setting asociado 2 codigo error(no importa) 3 mensaje de error
+            add_settings_error('portfolios_filtro','portfolios_orden_error','Solo dos opciones en el filtro.');
+            return get_option('portfolios_filtro');
+        }
+        return $input;
+    }
+    function sanitizeTextArea($input){
+        $lista_categorias = get_terms( array('taxonomy' => 'portfolios_category','hide_empty' => false ));
+        echo "<pre>";
+        var_dump($lista_categorias);
+        echo "</pre>";
+        echo "<pre>";
+        var_dump($input);
+        echo "</pre>";
+        $lista = explode(',',$input);
+        $lista = array_filter($lista);
+        echo "<pre>";
+        var_dump($lista);
+        echo "</pre>";
+        exit;
+        return $input;
+    }
 
 
   /////////////  BASE DE DATOS ///////////////////////
